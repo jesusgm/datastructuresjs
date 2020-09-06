5608e792472a24652537b81d77ba944b93df11ce
Jesús Gándara Martínez <jesusalxen@gmail.com>
2020-09-06 21:40:44 +0200

    fix: Added graph to general export at index

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const SingleLinkedLisst = require("./src/linked-lists/single-linked-list");
 const DoubleLinkedList = require("./src/linked-lists/double-linked-list");
 const Stack = require("./src/stack/stack");
-const Queue = require("./src/queue/queue")
+const Queue = require("./src/queue/queue");
+const Graph = require("./src/graph/graph");
 
 module.exports = {
     DoubleLinkedList,
+    Graph,
     SingleLinkedLisst,
     Stack,
     Queue
